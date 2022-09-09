41753a348d94c29f76710181fd779eccd322a5d0
Samcodedev <106853940+Samcodedev@users.noreply.github.com>
2022-09-09 23:29:11 +0100

    Update Header.jsx

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Header.css'
 import img from './img/Frame.png'
-{/* <iframe width="560" height="315" src="https://www.youtube.com/embed/hdw3KUed1Gs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */}
+
 function Header(){
     return(
         <div className='Header'>
